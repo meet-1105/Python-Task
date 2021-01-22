f96a1fbbda76b14d9f8c76cf5500a9d8da9313b1
meet-1105 <74321040+meet-1105@users.noreply.github.com>
2021-01-22 23:45:21 +0530

    Create consecutive.py

---
+++ consecutive.py
@@ -0,0 +1,30 @@
+def getMaxLength(arr, n): 
+  
+    # intitialize count 
+    count = 0 
+      
+    # initialize max 
+    result = 0 
+  
+    for i in range(0, n): 
+      
+        # Reset count when 0 is found 
+        if (arr[i] == 0): 
+            count = 0
+  
+        # If 1 is found, increment count 
+        # and update result if count  
+        # becomes more. 
+        else: 
+              
+            # increase count 
+            count = count + 1 
+            result = max(result, count)  
+          
+    return result  
+  
+# Driver code 
+arr = [0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1]  
+n = len(arr)  
+  
+print(getMaxLength(arr, n))
